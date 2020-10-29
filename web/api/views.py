34ff10efe6ac1039a0fddfbe1e5d6b7a9a4bbc05
fabry.ventura22@gmail.com <fabry.ventura22@gmail.com>
2020-10-29 01:14:11 -0300

    Api con funcionalidad de busqueda en endpoint de lista

--- web/api/views.py
+++ web/api/views.py
@@ -1,11 +1,16 @@
 from api.models import CableModem
 from api.serializers import CableModemSerializer
+from api.filters import CableModemFilter
+import django_filters.rest_framework
 from rest_framework import generics
 
 class CableModemList(generics.ListCreateAPIView):
     queryset = CableModem.objects.all()
     serializer_class = CableModemSerializer
-
+    filterset_class = CableModemFilter
+    def get_queryset(self):
+        args = self.request.query_params.dict()
+        return CableModem.objects.filter(**args)
 
 class CableModemDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = CableModem.objects.all()
--- web/web/settings.py
+++ web/web/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_filters',
     'api.apps.ApiConfig',
     'rest_framework',
     'django.contrib.admin',
@@ -106,7 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 # Internationalization
